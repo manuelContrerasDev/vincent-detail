9764eccea11e9e9c3c1692a5111765426282fb34
Manuel Contreras <tucorreo@ejemplo.com>
2026-05-05 17:07:06 -0400

    Limpia configuración de robots

--- app/robots.ts
+++ app/robots.ts
@@ -4,13 +4,10 @@ const siteUrl = "https://vincentdetail.cl";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-      },
-    ],
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
     sitemap: `${siteUrl}/sitemap.xml`,
-    host: siteUrl,
   };
 }
